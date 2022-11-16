067b1ff2f98f38c554a3cb71e124d0a7bdf58220
Lukman Asinmi <asinmilukman@gmail.com>
2022-11-16 08:22:04 -0800

    some errors fixed

--- exits.c
+++ exits.c
@@ -31,8 +31,8 @@ char *_strncpy(char *dest, char *src, int n)
 }
 
 /**
- **_strncat - concatenates two strings all
- *@dest: the first 
+ **_strncat - concatenates two strings
+ *@dest: the first string
  *@src: the second string
  *@n: the amount of bytes to be maximally used
  *Return: the concatenated string
